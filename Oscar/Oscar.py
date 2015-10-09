16807b793c24c124fed9f30ed939e606a0f58852
Vincentsan <vincent.gire@gmail.com>
2015-10-09 16:45:49 +0200

    Changed trial_id to trial_key. Added and exposed trial_id.

--- Oscar/Oscar.py
+++ Oscar/Oscar.py
@@ -15,6 +15,7 @@ class Oscar:
   def __init__(self, access_token):
     self.access_token = access_token
     self.trial_ids = {}
+    self.trial_keys = {}
 
   def call(self, url, params):
     # prepare request
@@ -36,22 +37,27 @@ class Oscar:
       print "Error when calling API:"
       raise e
 
-  def get_job_key(self, job):
+  def get_job_hash(self, job):
     return str(hash(json.dumps(job)))
+  
+  def get_job_id(self, job):
+    return self.trial_ids[self.get_job_hash(job)]
 
   def suggest(self, experiment):
     result = self.call('/suggest', {
                        'experiment' : json.dumps(experiment)
                        })
     # result is non nil or error has been raised
-    if 'job' in result and 'trial_id' in result:
-      self.trial_ids[self.get_job_key(result['job'])] = result['trial_id']
+    if 'job' in result and 'trial_key' in result and 'trial_id' in result:
+      hash = self.get_job_hash(result['job'])
+      self.trial_ids[hash] = result['trial_id']
+      self.trial_keys[hash] = result['trial_key']
       return result['job']
     else:
       raise ClientException("No job returned. Check your parameters and quotas " + json.dumps(result))
 
   def update(self, job, result):
     return self.call('/update', {
-                     'trial_id' : self.trial_ids[self.get_job_key(job)],
+                     'trial_key' : self.trial_keys[self.get_job_hash(job)],
                      'result' : json.dumps(result)
                      })
